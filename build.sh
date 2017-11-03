101aba230feb8d765701bdef1a927f3c8e49c24a
bmaupin <bmaupin@users.noreply.github.com>
2017-11-03 10:07:01 -0400

    Try out markdown + pandoc instead

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for texfile in *.tex; do
-    pdflatex $texfile
+# for texfile in *.tex; do
+#     pdflatex $texfile
+# done
+
+for mdfile in *.md; do
+    pandoc -S $mdfile -o ${mdfile/.md/.pdf}
 done
