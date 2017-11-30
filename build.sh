5a367d2a36cfea3e1389d95248fc4f3359f3bc4b
bmaupin <bmaupin@users.noreply.github.com>
2017-11-30 09:33:29 -0500

    Try to fix error in travis with chrome sandbox
    "The SUID sandbox helper binary was found, but is not configured correctly. Rather than run without sandboxing I'm aborting now. You need to make sure that /opt/google/chrome/chrome-sandbox is owned by root and has mode 4755"

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for htmlfile in *.html; do
-    google-chrome-stable --headless --disable-gpu --print-to-pdf file://$(pwd)/$htmlfile
+    google-chrome-stable --headless --no-sandbox --disable-gpu --print-to-pdf file://$(pwd)/$htmlfile
     mv output.pdf "${htmlfile%.html}.pdf"
 done
