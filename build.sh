427e0c73b399c1b6a698777fd3fb1d2e0c103a7b
bmaupin <bmaupin@users.noreply.github.com>
2021-02-18 14:27:17 -0500

    fix: Wait for fonts to load
    Thankfully --virtual-time-budget seems to be the upper limit and the build can finish sooner

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for htmlfile in *.html; do
-    google-chrome-stable --headless --no-sandbox --disable-gpu --print-to-pdf file://$(pwd)/$htmlfile
+    google-chrome-stable --headless --no-sandbox --disable-gpu --virtual-time-budget=10000 --print-to-pdf file://$(pwd)/$htmlfile
     mv output.pdf "${htmlfile%.html}.pdf"
 done
