582a536eefbfbd3f12ae9bdca583e778fec10fed
bmaupin <bmaupin@users.noreply.github.com>
2025-07-16 09:24:06 -0400

    refactor: Move source files to src/
    It's a nice convention and keeps the root directory less cluttered

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for htmlfile in *.html; do
+for htmlfile in src/*.html; do
     google-chrome-stable --headless --no-sandbox --disable-gpu --virtual-time-budget=10000 --print-to-pdf file://$(pwd)/$htmlfile
-    mv output.pdf "${htmlfile%.html}.pdf"
+    mv output.pdf $(basename "${htmlfile%.html}.pdf")
 done
